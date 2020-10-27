ee1e2b53a6d1a3d374f64b75992741e78268d86f
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 14:54:17 +0530

    displayed employee attendance status

--- empwage.sh
+++ empwage.sh
@@ -1,2 +1,10 @@
-echo "welcome to empwage computation pb"
-
+#!/bin/bash -x
+echo "welcome to employeewage pb"
+isPresent=1;
+randomcheck=$((RANDOM%2));
+if [ $ispresent -eq $randomcheck ]
+then
+     echo "employee is present"
+else
+    echo "employee is absent"
+fi
