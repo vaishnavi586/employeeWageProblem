2b6b948b6eb25065b33a2f3c4e5541a7cad67f3f
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 15:06:16 +0530

    displayed the emp wage adding part time

--- empwage.sh
+++ empwage.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -x
-echo "welcome to employeewage pb"
-isPresent=1;
-randomcheck=$((RANDOM%2));
-if [ $isPresent -eq $randomcheck ]
+echo "welcome to employeewage "
+isPresentfulltime=2;
+ispresentparttime=1;
+emprateperhr=20;
+randomcheck=$((RANDOM%3));
+if [ $isPresentfulltime -eq $randomcheck ]
 then
      emprateperhr=20;
      emphrs=8; 
      salary=$(($emphrs*$emprateperhr));
       echo $salary
      echo "employee is present"
-else 
-      echo "salary =0"
+elif [ $isPresentparttime -eq $randomcheck ]
+then
+     emphrs=4;
+     salary=$(($emphrs*$emprateperhr));
+     echo $salary
+else
+    echo "salary=0"
     echo "employee is absent"
+
 fi
