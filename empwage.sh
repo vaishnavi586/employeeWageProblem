c250eba8e3bc09a6478b9a83ad39b6a8d47a9771
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 20:24:37 +0530

    displayed day and dailywage

--- empwage.sh
+++ empwage.sh
@@ -1,50 +1,35 @@
-
-#!/bin/bash
-echo "welcome to employeewage "
-isPresentfulltime=2;
-ispresentparttime=1;
-emprateperhr=20;
+ispresentPartTime=1
+ispresentFullTime=2
+empRatePerHr=20
+TotalWorkingDays=20
 WorkingHours=100
-WorkingDays=20
 totalEmpHrs=0
 totalWorkingDays=0
 function getWorkingHours() {
-
-case $1 in
-    $ispresentfulltime)
-     emprateperhr=20;
-     emphrs=8;
-     salary=$(($emphrs*$emprateperhr));
-      echo $salary
-     echo "employee is present"
-;;
-    $ispresentparttime)
-     emphrs=4;
-     salary=$(($emphrs*$emprateperhr));
-     echo $salary
-    ;;
-    *)
-     emphrs=0;
-    echo "salary=0"
-    echo "employee is absent"
-
-;;
-esac
+        case $1 in
+                $ispresentPartTime)
+                        empHrs=4
+                        ;;
+                $ispresentFullTime)
+                        empHrs=8
+   			;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+	echo $empHrs
 }
 function getDailyWages() {
         totalWorkHours=$1
-        wages=$(( $totalWorkHours*$emprateperhr ))
+        wages=$(( $totalWorkHours*$empRatePerHr ))
         echo $wages
-
-
-
 }
- while [[ $totalEmpHrs -le $WorkingHours && $totalWorkingDays -le $WorkingDays ]]
+while [[ $totalEmpHrs -lt $WorkingHours && $totalWorkingDays -lt $TotalWorkingDays ]]
 do
-totalWorkingDays=$(($totalWorkingDays+1))
-emphrs=$(getWorkingHours $((RANDOM%3)) )
-totalEmpHrs=$(($totalEmpHrs+$emphrs))
- DailyWages[$totalWorkingDays]="$( getDailyWages $emphrs )"
+        totalWorkingDays=$(($totalWorkingDays+1))
+	empHrs=$(getWorkingHours $((RANDOM%3)) )
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        DailyWages[$totalWorkingDays]="$( getDailyWages $empHrs )"
 done
 totalSalary="$( getDailyWages $totalEmpHrs )"
 echo "daily wages:" ${DailyWages[@]}
